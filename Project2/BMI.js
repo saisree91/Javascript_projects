2fda0590ee664a8077544fbb93fe0ce003c276e6
saisree91 <saisreevakada9123@gmail.com>
2025-04-20 16:29:47 +0000

    project2

--- Project2/BMI.js
+++ Project2/BMI.js
@@ -5,19 +5,19 @@ container.addEventListener('submit', (e) => {
     e.preventDefault();
     const height = parseInt(document.querySelector("#height").value);
     const weight = parseInt(document.querySelector("#weight").value);
-    const res= document.querySelector(".result");
+    const result= document.querySelector(".result");
 
     
 
    
     if(height === "" || height < 0 || isNaN(height)){
-        res.innerHTML = "Please enter valid height";
+        result.innerHTML = "Please enter valid height";
     }
     else if(weight === "" || weight < 0 || isNaN(weight)){
-        res.innerHTML = "Please enter valid weight";
+        result.innerHTML = "Please enter valid weight";
     }
     else{
        const BMI = (weight/ ((height*height)/10000)).toFixed(2);
-       res.innerHTML = `${BMI}`;
+       result.innerHTML = `${BMI}`;
     }
 });
